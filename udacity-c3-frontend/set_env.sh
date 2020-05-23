cd973cc8711b993b644918bd2a376f482b0b7240
AreebSiddiqui <areeb40@hotmail.com>
2020-05-23 07:03:01 +0500

    Initial commit

---
+++ udacity-c3-frontend/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagrammasdev
+export POSTGRES_PASSWORD=udagrammasdev
+export POSTGRES_HOST=udagrammasdev.cddbmcdivsbn.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagrammasdev
+export AWS_BUCKET=p3-areeb-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
---
+++ udacity-c3-restapi-feed/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRESS_USERNAME=udagrammasdev
+export POSTGRESS_PASSWORD=udagrammasdev
+export POSTGRESS_HOST=udagrammasdev.cddbmcdivsbn.us-east-2.rds.amazonaws.com
+export POSTGRESS_DB=udagrammasdev
+export AWS_BUCKET=p3-areeb-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
+export aws_secret=vilxmX/UEYvAJ4eeSBn0HJVjwTqQ5Eoal+RFckkU
